3dbe9dcb1bc54abda356e8ef702cab0e7b7f3f00
Kosukeroku <tiltingatwindmills@yandex.ru>
2026-03-07 19:46:12 +0300

    UPGRB-1. Создать скелет приложения

---
+++ src/test/java/kosukeroku/ms_account_reservation/MsAccountReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package kosukeroku.ms_account_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAccountReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
